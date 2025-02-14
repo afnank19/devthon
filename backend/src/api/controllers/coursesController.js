09dd0f44d04f6088dba74952ccb686ee4c6d8a42
sharjeelch <sharjeelh6451@gmail.com>
2025-02-14 14:22:55 +0500

    2nd one

---
+++ backend/src/api/controllers/coursesController.js
@@ -0,0 +1,5 @@
+export const createCourse = async (req, res, next) => {
+ try {
+    
+ } catch (error) {}
+};
--- backend/src/api/routes/sessionRoutes.js
+++ backend/src/api/routes/sessionRoutes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { createCourse } from "../controllers/coursesController";
 
 const router = Router();
 
 router.route("/").get().post();
+
+// add course
+router.route("/:id").post(createCourse);
 
 export default router;
