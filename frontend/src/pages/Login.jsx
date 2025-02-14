839b78caa1d07d907810d2427248156b8c192dae
nainu <ammadanwar416@gmail.com>
2025-02-14 16:47:33 +0500

    feedback

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export default function Login() {
+  const [cmsId, setCmsId] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role , setRole] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
   return (
     <div>
       Login 
